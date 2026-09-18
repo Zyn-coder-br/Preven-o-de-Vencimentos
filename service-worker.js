11fd46fb486dc7eadf872ddc1741705f2c8160d5
Zyn-coder-br <iramonngomes@gmail.com>
2026-09-17 22:42:12 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='vencimentos-pa-v3.13.3.3-cache-v1';
-const ASSETS=['./','./index.html','./manifest.json','./service-worker.js'];
+const CACHE='vencimentos-pa-v3.13.3.5-cache-v1';
+const ASSETS=['./','./index.html','./manifest.json','./service-worker.js','./logo-pa.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const url=new URL(e.request.url);if(url.pathname.endsWith('/index.html')||url.pathname==='/'||url.pathname.endsWith('/manifest.json')||url.pathname.endsWith('/service-worker.js')){e.respondWith(fetch(e.request,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
